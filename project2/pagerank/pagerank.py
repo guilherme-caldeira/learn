5b4cd8b163874bde41e484958f7bb7af2e27c075
Guilherme Caldeira <gcaldeir@gmail.com>
2020-11-17 21:54:56 -0300

    sample_pagerank implemented

--- project2/pagerank/pagerank.py
+++ project2/pagerank/pagerank.py
@@ -5,16 +5,16 @@ import sys
 
 import pandas as pd
 import numpy as np
+from collections import Counter
 
 DAMPING = 0.85
 SAMPLES = 10000
 
 
 def main():
-    #if len(sys.argv) != 2:
-    #    sys.exit("Usage: python pagerank.py corpus")
-    #corpus = crawl(sys.argv[1])
-    corpus = crawl("corpus0")
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python pagerank.py corpus")
+    corpus = crawl(sys.argv[1])
     ranks = sample_pagerank(corpus, DAMPING, SAMPLES)
     print(f"PageRank Results from Sampling (n = {SAMPLES})")
     for page in sorted(ranks):
@@ -68,9 +68,9 @@ def transition_model(corpus, page, damping_factor):
     
     for pagex in corpus:
         if pagex in corpus[page]:
-            prob = (DAMPING / number_links) + ((1 - DAMPING) / number_pages_corpus)
+            prob = (damping_factor / number_links) + ((1 - damping_factor) / number_pages_corpus)
         else:
-            prob = (1 - DAMPING) / number_pages_corpus
+            prob = (1 - damping_factor) / number_pages_corpus
         prob_dist[pagex] = prob
 
     return prob_dist
@@ -85,7 +85,52 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    # List of pages
+    pages = []
+
+    # For the first time, choose a random page
+    corpus_page = []
+    for x in corpus:
+        corpus_page.append(x)
+    page = random.choice(corpus_page)
+    pages.append(page)
+
+    # List of pages to choose and their weight
+    pages_to_choose = []
+    pages_choose_weight = []
+
+    trans_mod = {}
+
+    # Repeat sampling N - 1 times because one page was already chosen
+    for i in range(n - 1):
+        # Get the transition model
+        trans_mod = transition_model(corpus, page, damping_factor)
+
+        # Loop through all items in the dictionary and add the key and the value to a list
+        for x, y in trans_mod.items():
+            pages_to_choose.append(x)
+            pages_choose_weight.append(y)
+        
+        # Draw a new page based on the transition model
+        new_page = random.choices(pages_to_choose, weights=pages_choose_weight, k=1)
+
+        # Add the chosen page to the list with all pages chosen
+        pages.append(new_page[0])
+
+        # Clear all collections
+        trans_mod.clear()
+        pages_to_choose.clear()
+        pages_choose_weight.clear()
+    
+    # Summarize
+    final_score = {}
+    final_score = Counter(pages)
+
+    # Gets the proportional value
+    for x, y in final_score.items():
+        final_score[x] = y / n
+
+    return final_score
 
 
 def iterate_pagerank(corpus, damping_factor):
